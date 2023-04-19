b2d9e62d7b0998121a4cc5e4e30e3a84acbfc63c
Eugeni Grivtsov <egivtsov@gmail.com>
2023-04-19 23:27:11 +0300

    redoc display issue fix v2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 MEDIA_URL = '/media/'
 
